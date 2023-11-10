7f96bb3909344e71877921155c873987ce73fbda
moznion <moznion@mail.moznion.net>
2023-11-10 15:14:29 +0900

    Revise test cases
    Signed-off-by: moznion <moznion@mail.moznion.net>

--- app_test.go
+++ app_test.go
@@ -6,12 +6,11 @@ import (
 	"strings"
 	"testing"
 
-	cp "github.com/otiai10/copy"
 	"github.com/stretchr/testify/assert"
 )
 
 func rollbackTestRepo() error {
-	cmd := exec.Command("git", "reset", "--hard", "30a414b283d1aca97b18ae867ffc55f0fcc960b4")
+	cmd := exec.Command("git", "reset", "--hard", "b6f4744e3813777569a9ac390a5c2608b800bfb7")
 	cmd.Dir = "./git-theseus-test-repo/"
 	err := cmd.Run()
 	if err != nil {
@@ -27,9 +26,7 @@ func TestApp(t *testing.T) {
 	err := rollbackTestRepo()
 	assert.NoError(t, err)
 
-	err = cp.Copy("git-theseus-test-repo/foo", "git-theseus-test-repo/foo_new")
-	assert.NoError(t, err)
-	err = cp.Copy("git-theseus-test-repo/bar", "git-theseus-test-repo/bar_new")
+	err = exec.Command("sh", "git-theseus-test-repo/code_generator.sh").Run()
 	assert.NoError(t, err)
 
 	cmd := exec.Command("go", "run", "../cmd/git-theseus/main.go", "--input-file", "git-theseus.json")
@@ -44,12 +41,12 @@ func TestApp(t *testing.T) {
 	cmd.Dir = "./git-theseus-test-repo/"
 	out, err := cmd.Output()
 	assert.NoError(t, err)
-	assert.Regexp(t, regexp.MustCompile(`commit [0-9a-f]{40}
+	assert.Regexp(t, regexp.MustCompile(`^commit [0-9a-f]{40}
 Author:     moznion <moznion@mail.moznion.net>
 AuthorDate: Wed Sep 27 18:56:49 2023 [+]0900
 Commit:     `+strings.TrimSpace(string(gitUserName))+" <"+strings.TrimSpace(string(gitEmail))+`>
 CommitDate: .+
-
+\s*
     \[git-theseus\] First commit
 \s*
     git-theseus does this migration commit.
@@ -60,31 +57,33 @@ new file mode 100644
 index 0000000..[0-9a-f]{7}
 --- /dev/null
 [+]{3} b/bar_new
-@@ -0,0 [+]1 @@
-[+]3
+@@ -0,0 [+]1,2 @@
+[+]3-A # original file's line is 3
+[+]3-B # original file's line is 3
 \\ No newline at end of file
 diff --git a/foo_new b/foo_new
 new file mode 100644
 index 0000000..[0-9a-f]{7}
 --- /dev/null
 [+]{3} b/foo_new
-@@ -0,0 [+]1,3 @@
-[+]1
-[+]2
-[+]3
+@@ -0,0 [+]1,4 @@
+[+]1-A # original file's line is 1
+[+]1-B # original file's line is 1
+[+]2-A # original file's line is 2
+[+]3-A # original file's line is 3
 \\ No newline at end of file
-`), string(out))
+$`), string(out))
 
 	cmd = exec.Command("git", "show", "HEAD^", "--pretty=fuller")
 	cmd.Dir = "./git-theseus-test-repo/"
 	out, err = cmd.Output()
 	assert.NoError(t, err)
-	assert.Regexp(t, regexp.MustCompile(`commit [0-9a-f]{40}
+	assert.Regexp(t, regexp.MustCompile(`^commit [0-9a-f]{40}
 Author:     dummy <dummy@example.com>
 AuthorDate: Wed Sep 27 18:57:22 2023 [+]0900
 Commit:     `+strings.TrimSpace(string(gitUserName))+" <"+strings.TrimSpace(string(gitEmail))+`>
 CommitDate: .+
-
+\s*
     \[git-theseus\] Second commit
 \s*
     git-theseus does this migration commit.
@@ -94,27 +93,29 @@ diff --git a/foo_new b/foo_new
 index [0-9a-f]{7}..[0-9a-f]{7} 100644
 --- a/foo_new
 [+]{3} b/foo_new
-@@ -1,3 [+]1,5 @@
- 1
- 2
--3
+@@ -1,4 [+]1,7 @@
+ 1-A # original file's line is 1
+ 1-B # original file's line is 1
+ 2-A # original file's line is 2
+-3-A # original file's line is 3
 \\ No newline at end of file
-[+]3
-[+]5
-[+]6
+[+]3-A # original file's line is 3
+[+]5-A # original file's line is 5
+[+]5-B # original file's line is 5
+[+]6-A # original file's line is 6
 \\ No newline at end of file
-`), string(out))
+$`), string(out))
 
 	cmd = exec.Command("git", "show", "HEAD", "--pretty=fuller")
 	cmd.Dir = "./git-theseus-test-repo/"
 	out, err = cmd.Output()
 	assert.NoError(t, err)
-	assert.Regexp(t, regexp.MustCompile(`commit [0-9a-f]{40}
+	assert.Regexp(t, regexp.MustCompile(`^commit [0-9a-f]{40}
 Author:     moznion <moznion@mail.moznion.net>
 AuthorDate: Wed Sep 27 18:57:38 2023 [+]0900
 Commit:     `+strings.TrimSpace(string(gitUserName))+" <"+strings.TrimSpace(string(gitEmail))+`>
 CommitDate: .+
-
+\s*
     \[git-theseus\] Third commit
 \s*
     git-theseus does this migration commit.
@@ -124,22 +125,24 @@ diff --git a/bar_new b/bar_new
 index [0-9a-f]{7}..[0-9a-f]{7} 100644
 --- a/bar_new
 [+]{3} b/bar_new
-@@ -1 [+]1,3 @@
-[+]1
-[+]2
- 3
+@@ -1,2 [+]1,4 @@
+[+]1-A # original file's line is 1
+[+]2-A # original file's line is 2
+ 3-A # original file's line is 3
+ 3-B # original file's line is 3
 \\ No newline at end of file
 diff --git a/foo_new b/foo_new
 index [0-9a-f]{7}..[0-9a-f]{7} 100644
 --- a/foo_new
 [+]{3} b/foo_new
-@@ -1,5 [+]1,6 @@
- 1
- 2
- 3
-[+]4
- 5
- 6
+@@ -2,6 [+]2,7 @@
+ 1-B # original file's line is 1
+ 2-A # original file's line is 2
+ 3-A # original file's line is 3
+[+]4-A # original file's line is 4
+ 5-A # original file's line is 5
+ 5-B # original file's line is 5
+ 6-A # original file's line is 6
 \\ No newline at end of file
-`), string(out))
+$`), string(out))
 }
